df46675fc9a7d9f590ff05fa4a607f3f8c8be971
ryaoi <ryaoi@student.42.fr>
2017-04-23 06:42:17 +0200

    Add files via upload

--- battleship.py
+++ battleship.py
@@ -1,16 +1,27 @@
 from random import randint
 
-board = []
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+board1 = []
+board2 = []
 
 for x in range(5):
-	board.append(["O"] * 5)
+    board1.append(["O"] * 5)
+    board2.append(["O"] * 5)
 
 def print_board(board):
 	for row in board:
 		print " ".join(row)
 
 print "Let's play Battleship!"
-print_board(board)
 
 def random_row(board):
     return randint(0, len(board) - 1)
@@ -18,18 +29,34 @@ def random_row(board):
 def random_col(board):
     return randint(0, len(board[0]) - 1)
 
-ship_row = random_row(board)
-ship_col = random_col(board)
+ship_row1 = random_row(board1)
+ship_col1 = random_col(board1)
+ship_row2 = random_row(board2)
+ship_col2 = random_col(board2)
+
 """
-print ("---The answer---\nrow:%d\ncol:%d\n----------------") % (ship_row, ship_col)
+print ("---The answer---\nrow1:%d\ncol1:%d\n----------------") % (ship_row1, ship_col1)
+print ("---The answer---\nrow2:%d\ncol2:%d\n----------------") % (ship_row2, ship_col2)
+
 """
-for turn in range(4):
-    print ("Turn:%d") % (turn + 1)
+for turn in range(8):
+    print bcolors.HEADER + ("Turn:%d Player:%d") % ((turn + 1) / 2, (turn) % 2 + 1) + bcolors.ENDC
+    if ((turn + 1) % 2 == 1):
+        board = board1
+        ship_row = ship_row1
+        ship_col = ship_col1
+    else:
+        board = board2
+        ship_row = ship_row2
+        ship_col = ship_col2
     get_row = raw_input("Guess Row:")
     get_col = raw_input("Guess Col:")
 	
     if(get_col == "" or get_row == ""):
-        print ("Missing input...")
+        if (turn == 7):
+			print  bcolors.FAIL + ("Game Over") + bcolors.ENDC
+        else:
+            print ("Missing input...")
         continue
     guess_row = int(get_row)
     guess_col = int(get_col)
@@ -45,6 +72,11 @@ for turn in range(4):
         else:
             print ("You missed my battleship!")
             board[guess_row][guess_col] = "X"
+            if ((turn + 1) % 2 == 1):
+                print bcolors.OKBLUE
+            else:
+                print bcolors.OKGREEN
             print_board(board)
-        if (turn == 3):
-            print ("Game Over")
+            print bcolors.ENDC
+        if (turn == 7):
+            print bcolors.FAIL + ("Game Over for both of you") + bcolors.ENDC
